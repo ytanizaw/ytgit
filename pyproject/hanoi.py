5333a3374f407fc59757648244a875c60562f0c3
ytanizaw <ytanizaw@nig.ac.jp>
2013-01-03 23:07:55 +0900

    format PEP
    format using sublime

--- pyproject/hanoi.py
+++ pyproject/hanoi.py
@@ -1,40 +1,42 @@
 #! coding:utf8
+
+
 class Hanoi:
     def __init__(self, n):
-        self.height =n
-        self.left=range(n,0,-1)
-        self.center=[]
-        self.right=[]
+        self.height = n
+        self.left = range(n, 0, -1)
+        self.center = []
+        self.right = []
 
     def show(self):
-        def getNth(x,n):
+        def getNth(x, n):
             try:
                 return x[n]
             except IndexError:
                 return 0
-        
-        n=self.height
-        ret=[]
+
+        n = self.height
+        ret = []
         for i in range(n):
-            ret.append((getNth(self.left,i), getNth(self.center,i) ,getNth(self.right,i)))
+            ret.append((getNth(self.left, i), getNth(self.center, i), getNth(self.right, i)))
         ret.reverse()
 
-        seps=3
+        seps = 3
         for row in ret:
-            line =""
+            line = ""
             for i in row:
-                if i==0:
-                    line+="　"*(n*2+1) +"　"*seps
+                if i == 0:
+                    line += "　" * (n * 2 + 1) + "　" * seps
                 else:
-                    line += "　"*(n-i+1) + "■"*(i*2-1) +"　"*(n-i+1)  +"　"*seps
-            line=line[:-seps]
+                    line += "　" * (n - i + 1) + "■" * (i * 2 - 1) + "　" * (n - i + 1) + "　" * seps
+            line = line[:-seps]
             print line
         print
 
     def solve(self, n, from_, to, via):
         def move(from_, to):
-            FROM=getattr(self, from_)
-            TO=getattr(self, to)
+            FROM = getattr(self, from_)
+            TO = getattr(self, to)
             TO.append(FROM.pop())
 
         if n == 1:
@@ -42,23 +44,21 @@ class Hanoi:
             self.show()
             print "%s => %s" % (from_, to)
         else:
-            self.solve( n - 1, from_, via, to)
+            self.solve(n - 1, from_, via, to)
             move(from_, to)
             self.show()
             print "%s => %s" % (from_, to)
             self.solve(n - 1, via, to, from_)
 
 
-hanoi=Hanoi(5)
+hanoi = Hanoi(8)
 hanoi.show()
-hanoi.solve(5,"left","center","right")
-
+hanoi.solve(8, "left", "right", "center")
 
 
 def get_hanoiNum(n):
-    if n==1:
+    if n == 1:
         return 1
     else:
-        return 2*get_hanoiNum(n-1) +1
-print get_hanoiNum(64)
-
+        return 2 * get_hanoiNum(n - 1) + 1
+print get_hanoiNum(8)
